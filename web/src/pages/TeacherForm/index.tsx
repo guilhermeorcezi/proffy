237e27887c468fb6d21b4e47f7bb5acec42f5619
Guilherme Orcezi <guilhermeorcezi@gmail.com>
2020-08-03 12:19:07 -0300

    first commit w/ web Landing

---
+++ web/src/pages/TeacherForm/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function TeacherForm() {
+	return <h1>TeacherForm</h1>;
+}
---
+++ web/src/pages/TeacherList/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function TeacherList() {
+	return <h1>Teachers</h1>;
+}
